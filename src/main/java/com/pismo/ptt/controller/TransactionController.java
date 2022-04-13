7d97aef18aede53878362a5cb7564e57c52f957f
Marcos Teixeira <marcos.teixeira.ti@gmail.com>
2022-04-13 03:14:10 -0300

    Preciso de uma Service?

--- src/main/java/com/pismo/ptt/controller/TransactionController.java
+++ src/main/java/com/pismo/ptt/controller/TransactionController.java
@@ -1,7 +1,5 @@
 package com.pismo.ptt.controller;
 
-import java.time.LocalDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pismo.ptt.config.ApplicationConfigConstants;
 import com.pismo.ptt.dto.TransactionDTO;
 import com.pismo.ptt.model.Transaction;
-import com.pismo.ptt.repository.AccountRepository;
 import com.pismo.ptt.repository.TransactionRepository;
-import com.pismo.ptt.utils.converters.OperationTypeConverter;
-import com.pismo.ptt.utils.enums.OperationTypeEnum;
-import com.pismo.ptt.utils.exceptions.AccountNotFoundException;
+import com.pismo.ptt.service.TransactionService;
 
 import io.swagger.v3.oas.annotations.Operation;
 
@@ -26,7 +21,7 @@ import io.swagger.v3.oas.annotations.Operation;
 public class TransactionController {
 
 	@Autowired
-	private AccountRepository accountRepository;
+	private TransactionService transactionService;
 
 	@Autowired
 	private TransactionRepository transactionRepository;
@@ -34,37 +29,10 @@ public class TransactionController {
 	@Operation(summary = "Criação de uma nova transação.")
 	@PostMapping("")
 	public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transactionDTO) {
-		Transaction t = this.transactionRepository.save(createClassFromDTO(transactionDTO));
+		Transaction t = this.transactionRepository.save(transactionService.createTransactionFromDTO(transactionDTO));
 		return new ResponseEntity<>(t, HttpStatus.CREATED);
 	}
 
-	private Transaction createClassFromDTO(TransactionDTO transactionDTO) {
-		Transaction newTransaction = new Transaction();
-		
-		try {
-			newTransaction.setAccount(accountRepository.findById(transactionDTO.getAccountId())
-					.orElseThrow(() -> new AccountNotFoundException()));
-		} catch (AccountNotFoundException e) {
-			e.printStackTrace();
-			return null;
-		}
-		
-		setOperationTypeAndAmount(transactionDTO, newTransaction);
-		newTransaction.setEventDate(LocalDateTime.now());
-		
-		return newTransaction;
-	}
-
-	private void setOperationTypeAndAmount(TransactionDTO transactionDTO, Transaction newTransaction) {
-		OperationTypeEnum operationType = new OperationTypeConverter().convertToEntityAttribute(transactionDTO.getOperationTypeId());
-		
-		newTransaction.setOperationTypeId(operationType);
-		
-		if(operationType.getOperationPositive()) {
-			newTransaction.setAmount(transactionDTO.getAmount());
-		} else {
-			newTransaction.setAmount(transactionDTO.getAmount().negate());
-		}
-	}
+	
 
 }
--- src/main/java/com/pismo/ptt/service/TransactionService.java
+++ src/main/java/com/pismo/ptt/service/TransactionService.java
@@ -1,8 +1,52 @@
 package com.pismo.ptt.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.pismo.ptt.dto.TransactionDTO;
+import com.pismo.ptt.model.Transaction;
+import com.pismo.ptt.repository.AccountRepository;
+import com.pismo.ptt.utils.converters.OperationTypeConverter;
+import com.pismo.ptt.utils.enums.OperationTypeEnum;
+import com.pismo.ptt.utils.exceptions.AccountNotFoundException;
 
 @Service
 public class TransactionService {
+	
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	
+	public Transaction createTransactionFromDTO(TransactionDTO transactionDTO) {
+		Transaction newTransaction = new Transaction();
+		
+		try {
+			newTransaction.setAccount(accountRepository.findById(transactionDTO.getAccountId())
+					.orElseThrow(() -> new AccountNotFoundException()));
+		} catch (AccountNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		setOperationTypeAndAmount(transactionDTO, newTransaction);
+		newTransaction.setEventDate(LocalDateTime.now());
+		
+		return newTransaction;
+	}
+
+	
+	private void setOperationTypeAndAmount(TransactionDTO transactionDTO, Transaction newTransaction) {
+		OperationTypeEnum operationType = new OperationTypeConverter().convertToEntityAttribute(transactionDTO.getOperationTypeId());
+		
+		newTransaction.setOperationTypeId(operationType);
+		
+		if(operationType.getOperationPositive()) {
+			newTransaction.setAmount(transactionDTO.getAmount());
+		} else {
+			newTransaction.setAmount(transactionDTO.getAmount().negate());
+		}
+	}
 
 }
